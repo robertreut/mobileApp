c66dfc82a11f5c668f37667b045f501e091f7721
Robert Reut <reutrobert96@yahoo.com>
2017-12-09 00:01:17 +0200

    added local storage

--- Android/CarPark/app/src/main/java/com/example/robert/carpark/MainActivity.java
+++ Android/CarPark/app/src/main/java/com/example/robert/carpark/MainActivity.java
@@ -1,33 +1,62 @@
 package com.example.robert.carpark;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+import com.example.robert.carpark.Pin;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     GoogleMap mGoogleMap;
+    GoogleApiClient mGoogleApiClient;
+    List<Pin> Pins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(googleServicesAvailable()) {
+        if (googleServicesAvailable()) {
             Toast.makeText(this, "Perfect!!!", Toast.LENGTH_LONG).show();
             setContentView(R.layout.activity_main);
             initMap();
+            //setMarkers();
         } else {
             //No google maps layout
+        }
+    }
+
+    private void setMarkers() {
+        for (int i = 0; i < Pins.size(); i++) {
+            setMarker(Pins.get(i).Locality, Pins.get(i).Latitude, Pins.get(i).Longitude, Pins.get(i).getAddress());
         }
     }
 
@@ -37,12 +66,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public boolean googleServicesAvailable() {
-        GoogleApiAvailability api= GoogleApiAvailability.getInstance();
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if(isAvailable == ConnectionResult.SUCCESS) {
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
         } else if (api.isUserResolvableError(isAvailable)) {
-            Dialog dialog= api.getErrorDialog(this,isAvailable,0);
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
             dialog.show();
         } else {
             Toast.makeText(this, "Can't Connect to play services", Toast.LENGTH_LONG).show();
@@ -53,7 +82,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        goToLocationZoom(46.769533, 23.590450,15);
+        //goToLocationZoom(46.769533, 23.590450,17);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(LocationServices.API)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .build();
+//        mGoogleApiClient.connect();
     }
 
     private void goToLocation(double lat, double lng) {
@@ -68,4 +115,122 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mGoogleMap.moveCamera(update);
     }
 
+    Marker marker;
+
+    public void geoLocate(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.editText2);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list = gc.getFromLocationName(location, 1);
+        android.location.Address address = list.get(0);
+        String locality = address.getLocality();
+        String adresa = address.getThoroughfare();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocationZoom(lat, lng, 17);
+
+        Pin pin = new Pin();
+        pin.setLocality(locality);
+        pin.setLatitude(lat);
+        pin.setLongitude(lng);
+        pin.setOccupied(true);
+        pin.setAddress(address.getThoroughfare());
+        //Pins.add(pin);
+
+        setMarker(locality, lat, lng, adresa);
+
+    }
+
+    private void setMarker(String locality, double lat, double lng, String adresa) {
+        if(marker != null){
+            marker.remove();
+        }
+
+        MarkerOptions options = new MarkerOptions()
+                            .title(locality)
+                            .position(new LatLng(lat, lng))
+                            .snippet(adresa)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+        marker = mGoogleMap.addMarker(options);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //TODO:
+        //add dialogs
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //TODO:
+        //add dialogs
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            Toast.makeText(this, "Can't get current location", Toast.LENGTH_LONG).show();
+        }else {
+            LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 16);
+            mGoogleMap.animateCamera(update);
+        }
+    }
 }
---
+++ Android/CarPark/app/src/main/java/com/example/robert/carpark/Pin.java
@@ -0,0 +1,54 @@
+package com.example.robert.carpark;
+
+/**
+ * Created by Robert on 08.12.2017.
+ */
+
+public class Pin {
+    String Locality;
+    Double Longitude;
+    Double Latitude;
+    Boolean Occupied;
+    String Address;
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public Boolean getOccupied() {
+        return Occupied;
+    }
+
+    public Double getLongitude() {
+        return Longitude;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public String getLocality() {
+        return Locality;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public void setLocality(String locality) {
+        this.Locality = locality;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.Longitude = longitude;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setOccupied(Boolean occupied) {
+        Occupied = occupied;
+    }
+
+}
